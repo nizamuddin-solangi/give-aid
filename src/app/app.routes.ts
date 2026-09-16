45d4d1a6c39a610e22f4f444fea9c1dccce9eeb5
nizamuddin-solangi <nizamuddinsolangi614@gmail.com>
2026-09-17 00:24:44 +0500

    alil changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ export const routes: Routes = [
  {path:'',pathMatch:'full',loadComponent:page('home'),data:{page:'home'}},
  {path:'donate',loadComponent:page('donate'),data:{page:'donate'}}, {path:'programmes',loadComponent:page('programmes'),data:{page:'programmes'}},
  {path:'help',loadComponent:page('help'),data:{page:'help'}}, {path:'about',loadComponent:page('about'),data:{page:'about'}},
+ {path:'support',loadComponent:page('support'),data:{page:'support'}}, {path:'transparency',loadComponent:page('transparency'),data:{page:'transparency'}},
  {path:'partners',loadComponent:page('partners'),data:{page:'partners'}}, {path:'gallery',loadComponent:page('gallery'),data:{page:'gallery'}},
  {path:'contact',loadComponent:page('contact'),data:{page:'contact'}}, {path:'register',loadComponent:page('register'),data:{page:'register'}},
  {path:'login',loadComponent:page('login'),data:{page:'login'}}, {path:'invite',loadComponent:page('invite'),data:{page:'invite'}}, {path:'**',redirectTo:''}
